5471cdfdfed7dfcb2cbc491b833340584f69eb81
Jean <kenmogne12@gmail.com>
2023-01-20 20:37:51 -0600

    Adding 2 public subnet

---
+++ public-subnet.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "public-subnet-1" {
+    cidr_block = "10.0.1.0/20"
+    vpc_id = aws_vpc.client-9-network.id
+    availability_zone = "us-east-1a"
+    tags = {
+      name = "client-9-network-subnet-public-subnet-1"
+    }
+  
+}
+
+resource "aws_subnet" "public-subnet-2" {
+    cidr_block = "10.0.3.0/20"
+    vpc_id = aws_vpc.client-9-network.id
+    availability_zone = "us-east-1b"
+    tags = {
+      name = "client-9-network-subnet-public-subnet-2"
+    }
+  
+}
