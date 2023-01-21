0c29c5f950dd800bd55785f4f0b2433331975ea6
Jean <kenmogne12@gmail.com>
2023-01-20 21:56:29 -0600

    modifying route table association spelling

--- private-route-table-association.tf
+++ private-route-table-association.tf
@@ -3,7 +3,7 @@ resource "aws_route_table_association" "private-route-table-association" {
     subnet_id = aws_subnet.private-subnet-1.id
   
 }
-resource "aws_route_table_association" "private-route-table-association" {
+resource "aws_route_table_association" "private-route-table-association-2" {
     route_table_id = aws_route_table.private-route-table.id
     subnet_id = aws_subnet.private-subnet-2.id
   
--- public-route-table-association.tf
+++ public-route-table-association.tf
@@ -3,7 +3,7 @@ resource "aws_route_table_association" "public-route-table-association" {
     subnet_id = aws_subnet.public-subnet-1.id
   
 }
-resource "aws_route_table_association" "public-route-table-association" {
+resource "aws_route_table_association" "public-route-table-association-1" {
     route_table_id = aws_route_table.public-route-table.id
     subnet_id = aws_subnet.public-subnet-2.id
   
